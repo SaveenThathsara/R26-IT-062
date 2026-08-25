a7cf16ae0926c5cd8d669b26620e94668cf05131
SThathsara-u <thathsara1221@gmail.com>
2026-08-25 14:04:57 +0530

    Add missing includes for generated plugin registrant

--- betel_app_quality_model/linux/flutter/generated_plugin_registrant.cc
+++ betel_app_quality_model/linux/flutter/generated_plugin_registrant.cc
@@ -4,7 +4,9 @@
 
 // clang-format off
 
+#include "generated_plugin_registrant.h"
 
+#include <file_selector_linux/file_selector_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) file_selector_linux_registrar =
